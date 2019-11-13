6a88bcd9618171503aa57b175b0c0ce9c613a0da
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-13 05:01:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -78,8 +78,14 @@ def gross_per_studio(collection)
   mov_row = 0 
   while mov_row < collection.length do 
     mov_name = collection[mov_row]
-    if !total[mov_name[:studio]] = mov_name[:worldwide_gross]
-      
+    if !total[mov_name[:studio]]
+      total[mov_name[:studio]] = mov_name[:worldwide_gross]
+    else 
+      total[mov_name[:studio]] += mov_name[:worldwide_gross]
+    end
+    mov_row += 1
+  end
+  total
 end
 
 def movies_with_directors_set(source)
